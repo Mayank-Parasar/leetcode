dab9f5edac6d06e96dd4be904834b4598d8d1607
Mayank Parasar <mayankparasar@gmail.com>
2020-01-11 12:25:46 -0500

    sorting algorithms++

--- sorting_algorithms.cpp
+++ sorting_algorithms.cpp
@@ -43,6 +43,29 @@ vector<int> bubble_sort(vector<int> a) {
     return a;
 }
 
+vector<int> insertion_sort(vector<int> a) {
+    // starting from size = 1, check if the subarray is sorted
+    // if not then insert the element at appropriate position
+    // in the subarray.
+    for(int ii=0; ii < a.size(); ii++) {
+        for(int kk=0; kk <= ii; kk++) {
+            // you  are always checking if the last element in the
+            // sub array is sorted or not
+            if(a[ii] < a[kk]) {
+                //  this is the condition to reorder the array
+                // sort this sub array by moving elements
+                // from ii-1 --> kk by one position
+                int tmp = a[ii];
+                for(int jj = ii-1; jj>=kk; jj-- ) {
+                    a[jj+1] = a[jj];
+                }
+                a[kk] = tmp;
+            }
+        }
+    }
+    return a;
+}
+
 int main() {
 
     vector<int> a = {2, 5, 1, 7, 9, 3, 6, 8, 5};
@@ -50,6 +73,7 @@ int main() {
 
     vector<int> selection_sorted = selection_sort(a);
     vector<int> bubble_sorted = bubble_sort(a);
+    vector<int> insertion_sorted = insertion_sort(a);
 
     cout << "Original vector" << endl;
     for(auto  i: a)
@@ -67,6 +91,13 @@ int main() {
     cout << "Output from bubble-sort" << endl;
 
     for(auto i : bubble_sorted)
+        cout << i << " ";
+
+    cout << endl;
+
+    cout << "Output from insertion-sort" << endl;
+
+    for(auto i : insertion_sorted)
         cout << i << " ";
 
     return 0;
