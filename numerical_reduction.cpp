c1dc07279e0fdb9c54d452d940a30f32bd3f4026
Mayank Parasar <mayankparasar@gmail.com>
2020-03-20 15:34:31 -0400

    code for numerically reducing a non-negative number to a single digit

---
+++ numerical_reduction.cpp
@@ -0,0 +1,55 @@
+//
+// Created by Mayank Parasar on 2020-03-20.
+//
+/*
+Code for reducing a numberr to a single digit
+175 =  5+7 => 12 => 1+2 = 3 + 1 => 4
+9999 = 9+9 =>9 + 9 => 9 =>9+9 = 9
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/* helper function */
+vector<int> vectorize (int num) {
+    vector<int> result;
+
+    while(num >0) {
+        result.push_back(num % 10);
+        num = num / 10;
+    }
+
+    return result;
+}
+
+
+int reduce(int num) {
+    vector<int> result = vectorize(num);
+    int sum = 0;
+    for(auto i : result) {
+        sum += i;
+        if(sum > 9) {
+            sum = reduce(sum);
+        }
+    }
+
+    return sum;
+}
+
+int main() {
+
+    // vector<int> vec = vectorize(175);
+
+    // for(auto i : vec) {
+    // 	cout << i << " ";
+    // }
+    cout << endl;
+    int reduced_sum = reduce(175);
+    cout << "result after reducing the number: " << reduced_sum << endl;
+    cout << endl;
+    reduced_sum = reduce(9999);
+    cout << "result after reducing the number: " << reduced_sum << endl;
+    return 0;
+}
